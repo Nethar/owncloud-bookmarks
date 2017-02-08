bdcde621e1051ec7e72a77d0aa3da137aba9c128
Christian Schabesberger <chris.schabesberger@mailbox.org>
2017-02-08 10:23:02 +0100

    use android 5.1+ permission api

--- app/src/main/java/cz/nethar/owncloudbookmarks/LoginActivity.java
+++ app/src/main/java/cz/nethar/owncloudbookmarks/LoginActivity.java
@@ -1,10 +1,19 @@
 package cz.nethar.owncloudbookmarks;
 
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import static cz.nethar.owncloudbookmarks.TagList.ACCOUNT_TYPE;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
@@ -16,16 +25,23 @@ public class LoginActivity extends AppCompatActivity {
      * The default email to populate the email field with.
      */
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
         
-/*        String text = "";    
-        AccountManager accountManager = AccountManager.get(getApplicationContext());
-        Account[] accounts = accountManager.getAccountsByType(ACCOUNT_TYPE);
-        
+        String text = "";
+
+        Log.d("asdfasdf", "alsdfkjasldkfjaslkfjaskfj");
+
+
+
+        // AccountManager accountManager = AccountManager.get(getApplicationContext());
+        // Account[] accounts = accountManager.getAccountsByType(ACCOUNT_TYPE);
+
+        /*
         if (accounts.length == 0) {
         	
         }
@@ -56,6 +72,7 @@ public class LoginActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
+
     
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
--- app/src/main/java/cz/nethar/owncloudbookmarks/SettingsActivity.java
+++ app/src/main/java/cz/nethar/owncloudbookmarks/SettingsActivity.java
@@ -1,8 +1,15 @@
 package cz.nethar.owncloudbookmarks;
 
 import cz.nethar.owncloudbookmarks.R;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,64 +25,81 @@ import android.content.SharedPreferences.Editor;
 
 public class SettingsActivity extends AppCompatActivity {
 
+	private static final String TAG = SettingsActivity.class.toString();
+
 	private String url, user, pass;
 	private boolean trust;
 	public static int sort;
-	
+
+	private static final int GET_PERMISSIONS_CALL = 42;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-		
-        SharedPreferences sp = getSharedPreferences(TagList.PREF_KEY, MODE_PRIVATE);      
+
+
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.GET_ACCOUNTS}, GET_PERMISSIONS_CALL);
+		} else {
+			initSettings();
+		}
+	}
+
+	private void initSettings() {
+		SharedPreferences sp = getSharedPreferences(TagList.PREF_KEY, MODE_PRIVATE);
 		url = sp.getString(TagList.OWNCLOUD_ADDRESS, "");
 		trust = sp.getBoolean(TagList.OWNCLOUD_ACCEPTWRONGCERT, false);
 		user = sp.getString(TagList.OWNCLOUD_USERNAME, "");
 		pass = sp.getString(TagList.OWNCLOUD_PASSWORD, "");
-		
-		if (url.length() == 0 && user.length() == 0) {
-	        AccountManager accountManager = AccountManager.get(getApplicationContext());
-	        Account[] accounts = accountManager.getAccountsByType("owncloud");
-	        
-	        if (accounts.length > 0) {
-	        	Account a = accounts[0];
-	        	String[] all = a.name.split("@");
-	        	if (all.length == 2) {
-	        		user = all[0];
-	        		url = all[1];
-	        		if (!url.contains("http")) {
-	        			url = "http://" + url;
-	        		}
-	        	}
-	        }
-		}
-        
-        EditText et = (EditText)this.findViewById(R.id.editTextUrl);
-        et.setText(url);
-        
-        CheckBox trustCH = (CheckBox)this.findViewById(R.id.trustCheckBox);
-        trustCH.setChecked(trust);
-        
-        et = (EditText)this.findViewById(R.id.editTextUser);
-        et.setText(user);
-        
-        et = (EditText)this.findViewById(R.id.editTextPass);
-        et.setText(pass);
-        
-        Spinner spinner = (Spinner)this.findViewById(R.id.SpinnerSort);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sorting_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
-        sort = sp.getInt(TagList.OWNCLOUD_SORTING, TagList.ALPHA_ASC);
-        spinner.setSelection(sort);
+
+		if (url.length() == 0 && user.length() == 0) {
+			AccountManager accountManager = AccountManager.get(getApplicationContext());
+			Account[] accounts = null;
+			try {
+				accounts = accountManager.getAccountsByType("owncloud");
+			} catch (SecurityException se) {
+				Log.e(TAG, "Could not get account access");
+				se.printStackTrace();
+				finish();
+				return;
+			}
+
+			if (accounts.length > 0) {
+				Account a = accounts[0];
+				String[] all = a.name.split("@");
+				if (all.length == 2) {
+					user = all[0];
+					url = all[1];
+					if (!url.contains("https")) {
+						url = "https://" + url;
+					}
+				}
+			}
+		}
+
+		EditText et = (EditText) this.findViewById(R.id.editTextUrl);
+		et.setText(url);
+
+		CheckBox trustCH = (CheckBox) this.findViewById(R.id.trustCheckBox);
+		trustCH.setChecked(trust);
+
+		et = (EditText) this.findViewById(R.id.editTextUser);
+		et.setText(user);
+
+		et = (EditText) this.findViewById(R.id.editTextPass);
+		et.setText(pass);
+
+		Spinner spinner = (Spinner) this.findViewById(R.id.SpinnerSort);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sorting_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+
+		sort = sp.getInt(TagList.OWNCLOUD_SORTING, TagList.ALPHA_ASC);
+		spinner.setSelection(sort);
 	}
 
-	// two for spinner
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-    }
-    public void onNothingSelected(AdapterView<?> parent) {
-    }
     
 	public void saveClick(View view) {
         SharedPreferences sp = getSharedPreferences(TagList.PREF_KEY, MODE_PRIVATE);      
@@ -129,6 +153,12 @@ public class SettingsActivity extends AppCompatActivity {
         }
 
 		finish();
+	}
+
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		initSettings();
 	}
 
 }
